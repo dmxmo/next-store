03850a4b6e247f329699449122847d7d844964b2
Daryl <daryl.maximo@gmail.com>
2023-01-23 00:34:33 -0800

    Test 2

--- app/[category]/ProductsList.js
+++ app/[category]/ProductsList.js
@@ -3,17 +3,18 @@ import styles from "./ProductsList.module.css";
 import ProductCard from "./ProductCard";
 import { isEmpty } from "lodash";
 export default function ProductsList(props) {
+  console.log(props)
   if (isEmpty(props.products) || isEmpty(props.category)) {
     return <p>No items found.</p>;
   }
   const productCards = props.products.map(product => {
-    return (
-      <ProductCard key={product.id} product={product} url={`${props.category.handle}/${product.handle}_${product.id}`} />
-    )
+    // return (
+    //   <ProductCard key={product.id} product={product} url={`${props.category.handle}/${product.handle}_${product.id}`} />
+    // )
   });
   return (
     <div className={styles.productsListContainer}>
-      {productCards}
+      {/* {productCards} */}
     </div>
   )
 }
