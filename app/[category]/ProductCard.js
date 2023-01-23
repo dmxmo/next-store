4b166b52a5e2a15f076aa4d6c4b389fe0658e368
Daryl <daryl.maximo@gmail.com>
2023-01-22 22:01:41 -0800

    Clean up

--- app/[category]/ProductCard.js
+++ app/[category]/ProductCard.js
@@ -1,12 +1,12 @@
 import styles from "./ProductCard.module.css";
 import Image from 'next/image'
 import Link from "next/link";
-export default function ProductCard({product, url}) {
+export default function ProductCard({ product, url }) {
   return (
     <div className={styles.productCard}>
-      <Link  href={url}>
-      <Image src={product.image.src} width={product.image.width} height={product.image.height} alt={product.title} />
-      <div className={styles.productName}>{product.title}</div>
+      <Link href={url}>
+        <Image src={product.image.src} width={product.image.width} height={product.image.height} alt={product.title} />
+        <div className={styles.productName}>{product.title}</div>
       </Link>
     </div>
   )
--- app/[category]/ProductsList.js
+++ app/[category]/ProductsList.js
@@ -1,7 +1,7 @@
 
 import styles from "./ProductsList.module.css";
 import ProductCard from "./ProductCard";
-export default function ProductsList({products, category}) {
+export default function ProductsList({ products, category }) {
   const productCards = products.map(product => {
     return (
       <ProductCard key={product.id} product={product} url={`${category.handle}/${product.handle}_${product.id}`} />
