591d08c5255c1505e34fdbf10fc91d22533d85d3
Daryl <daryl.maximo@gmail.com>
2023-01-23 23:57:46 -0800

    Revalidate menu every 10s, cache cat page

--- app/[category]/page.js
+++ app/[category]/page.js
@@ -17,8 +17,8 @@ async function fetchCategory(categoryHandle) {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': `${storeToken}`
     },
-    cache: 'no-cache',
-    next: { revalidate: 10 },
+    // cache: 'no-cache',
+    // next: { revalidate: 10 },
     agent
   });
   const data = await res.json();
