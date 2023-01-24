ac15b83a1816a7e5e9678debfdc0c774476e9338
Daryl <daryl.maximo@gmail.com>
2023-01-24 00:42:37 -0800

    test 10

--- app/Header.js
+++ app/Header.js
@@ -17,7 +17,7 @@ async function getCategories() {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': `${storeToken}`
     },
-    cache: 'no-cache',
+    // cache: 'no-cache',
     next: { revalidate: 10 },
     agent
   });
