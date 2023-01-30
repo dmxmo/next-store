0c54b0aac5b5814abb675acfd22cfbc0e23daa53
Daryl <daryl.maximo@gmail.com>
2023-01-29 17:14:59 -0800

    minor fix

--- app/redirecting-to-checkout/page.js
+++ app/redirecting-to-checkout/page.js
@@ -1,13 +1,13 @@
 'use client';
-import { getCookie } from 'cookies-next';
+import { hasCookie } from 'cookies-next';
 import Link from 'next/link';
 export default function RedirectToCheckOutPage() {
-  const lastCheckoutUrl = getCookie('lastCheckoutUrl');
+  const hasLastCheckoutUrl = hasCookie('lastCheckoutUrl');
   return (
     <>
       <h3>Redirecting to checkout...</h3>
       <br />
-      {!lastCheckoutUrl ? <p>There's no item in your cart. Go back <Link href="/">Home &gt;&gt;&gt;</Link></p> : <p>Please wait...</p>}
+      {hasLastCheckoutUrl ? <p>Please wait...</p> : <p>There's no item in your cart. Go back <Link href="/">Home &gt;&gt;&gt;</Link></p>}
     </>
   )
 }
