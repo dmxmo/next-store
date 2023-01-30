6c97681c061d11a46a17075bc6f41ef49b351177
Daryl <daryl.maximo@gmail.com>
2023-01-29 17:05:57 -0800

    Remove redirect to home

--- app/redirecting-to-checkout/page.js
+++ app/redirecting-to-checkout/page.js
@@ -1,14 +1,13 @@
 'use client';
 import { getCookie } from 'cookies-next';
+import Link from 'next/link';
 export default function RedirectToCheckOutPage() {
   const lastCheckoutUrl = getCookie('lastCheckoutUrl');
-  if (!lastCheckoutUrl) {
-    window.location.replace('/');
-  }
   return (
     <>
-      <h1>Redirecting to checkout...</h1>
-      <p>Please wait...</p>
+      <h3>Redirecting to checkout...</h3>
+      <br />
+      {!lastCheckoutUrl ? <p>There's no item in your cart. Go back <Link href="/">Home &gt;&gt;&gt;</Link></p> : <p>Please wait...</p>}
     </>
   )
 }
