addf436465f411cd0530a8a0d5ef989488a2562a
Daryl <daryl.maximo@gmail.com>
2023-01-29 17:53:21 -0800

    edit redirect msg

--- app/redirecting-to-checkout/page.js
+++ app/redirecting-to-checkout/page.js
@@ -1,13 +1,9 @@
-'use client';
-import { hasCookie } from 'cookies-next';
-import Link from 'next/link';
 export default function RedirectToCheckOutPage() {
-  const hasLastCheckoutUrl = hasCookie('lastCheckoutUrl');
   return (
     <>
       <h3>Redirecting to checkout...</h3>
       <br />
-      {hasLastCheckoutUrl ? <p>Please wait...</p> : <p>There's no item in your cart. Go back <Link href="/">Home &gt;&gt;&gt;</Link></p>}
+      <p>Please wait...</p>
     </>
   )
 }
